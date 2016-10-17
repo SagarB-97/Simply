551c7d3a6a3633656308f6c8a1ce164203a7d2ef
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2016-10-17 14:39:40 +0530

    Initial commit

---
+++ app/src/main/java/sagarb/simply/MainActivity.java
@@ -0,0 +1,20 @@
+package sagarb.simply;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView)findViewById(R.id.webv);
+        wv.loadUrl("http://slashdot.org/");
+    }
+}
